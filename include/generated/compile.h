19633ef63116a4bbdb49b8a64cf62e48fd450a0c
Serkan Kurt <serktheturk10@gmail.com>
2013-07-13 19:52:30 -0400

    original Samsung kernel 2.6.35

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Sat Jul 13 15:57:43 EDT 2013"
+#define LINUX_COMPILE_TIME "15:57:43"
+#define LINUX_COMPILE_BY "serkan"
+#define LINUX_COMPILE_HOST "serkan-Ubuntu"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
